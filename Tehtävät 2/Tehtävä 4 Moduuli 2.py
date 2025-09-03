aefc254f8bd0c696c215ad0e2a98a32d1b00dd70
Samuli Siurala <samppa.siurala@hotmail.com>
2025-09-03 14:10:52 +0300

    Moduuli 2 tehtävät

---
+++ Tehtävät 2/Tehtävä 4 Moduuli 2.py
@@ -0,0 +1,8 @@
+import math
+a = int(input("Anna 1. kokonaisluku: "))
+b = int(input("Anna 2. kokonaisluku: "))
+c = int(input("Anna 3. kokonaisluku: "))
+
+print("Summa:" , a + b + c)
+print("Tulo:" , a * b * c)
+print("Keskiarvo:" , (a + b + c) / 3)
