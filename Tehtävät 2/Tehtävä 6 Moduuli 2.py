72b32902a78b751057284375113c3c010af8c8c8
Samuli Siurala <samppa.siurala@hotmail.com>
2025-09-03 14:45:01 +0300

    Moduuli 2 tehtävät

---
+++ Tehtävät 2/Tehtävä 6 Moduuli 2.py
@@ -0,0 +1,13 @@
+import math
+import random
+
+satunnainen = random.randint(0, 9)
+print("Satunnainen luku:", satunnainen)
+
+satunnainen = random.randint(0, 9)
+print("Satunnainen luku:", satunnainen)
+
+satunnainen = random.randint(0, 9)
+print("Satunnainen luku:", satunnainen)
+
+
