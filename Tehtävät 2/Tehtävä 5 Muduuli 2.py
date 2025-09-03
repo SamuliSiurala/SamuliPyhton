81a54c561f7234807c72636cc144e6e5ea16e018
Samuli Siurala <samppa.siurala@hotmail.com>
2025-09-03 14:33:33 +0300

    Moduuli 2 tehtävät

--- Tehtävät 2/Tehtävä 5 Muduuli 2.py
+++ Tehtävät 2/Tehtävä 5 Muduuli 2.py
@@ -1,6 +1,17 @@
+LUOTI_GRAMMAA = 13.3
+NAULA_LUOTIA = 32
+LEIVISKA_NAULOJA = 20
 
 leiviskat = int(input("Anna leiviskät: "))
 naulat = int(input("Anna naulat: "))
 luodit = int(input("Anna luodit: "))
 
+kokonaisluodit = (leiviskat * LEIVISKA_NAULOJA * NAULA_LUOTIA +
+                  naulat * NAULA_LUOTIA + luodit)
 
+grammat = kokonaisluodit * LUOTI_GRAMMAA
+
+kilogrammat = int(grammat // 1000)
+grammat_jaljella = (grammat % 1000)
+
+print(f"Massa on yhteensä {kilogrammat} kilogrammaa ja {grammat_jaljella:.2f} grammaa.")
