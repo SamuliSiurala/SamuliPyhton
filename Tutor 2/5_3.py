d7dfa2a2a61b196e4e7e4353dc979794df7e90e4
Samuli Siurala <samppa.siurala@hotmail.com>
2025-10-17 17:28:36 +0300

    Tutor 2

--- Tutor 2/5_3.py
+++ Tutor 2/5_3.py
@@ -5,9 +5,11 @@ luvut = [2,3,5,7]
 while True:
     luku = int(input("Anna luku: "))
 
-    for testiluku in luvut:
+    for i in range(3):
 
-        if luku % testiluku == 0:
+        if luku % luvut[i] == 0:
             print("Ei ole alkuluku")
             break
+        elif luvut[i] == 5:
+            print("Alkuluku")
 
