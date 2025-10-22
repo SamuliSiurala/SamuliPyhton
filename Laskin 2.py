592d15ab9e0f6da8b61e600643ee08c17cef4aa6
Samuli Siurala <samppa.siurala@hotmail.com>
2025-10-22 17:52:10 +0300

    Laskin

--- Laskin 2.py
+++ Laskin 2.py
@@ -1,18 +1,26 @@
-print("Valitse mitä toimintoa haluat käyttää:´\n A: Yhteenlasku \n B: Vähennyslasku"
-      "\n C: Kertolasku \n D: Jakolasku")
+print("-----Tervetuloa käyttämään laskinta!-----")
 
-valinta = input("Valintasi (A-D): ").upper()
+while True:
+    print("Valitse mitä toimintoa haluat käyttää:´\n A: Yhteenlasku \n B: Vähennyslasku"
+          "\n C: Kertolasku \n D: Jakolasku")
 
-a = float(input("Anna ensimmäinen luku: "))
-b = float(input("Anna toinen luku: "))
+    valinta = input("Valintasi (A-D): ").upper()
 
-if valinta == "A":
-    print("Yhteenlasku:" , a + b)
-elif valinta == "B":
-    print("Vähennyslasku:" , a - b)
-elif valinta == "C":
-    print("Kertolasku:" , a * b)
-elif valinta == "D":
-    print("Desimaalijakolasku:" , a / b)
-else:
-    print("Valintasi on virheellinen.")
+    #While loopin katkaisu
+    if valinta == "Q":
+        print("Ohjelma lopetetaan, kiitos!")
+        break
+
+    a = float(input("Anna ensimmäinen luku: "))
+    b = float(input("Anna toinen luku: "))
+
+    if valinta == "A":
+        print("Yhteenlasku:" , a + b)
+    elif valinta == "B":
+        print("Vähennyslasku:" , a - b)
+    elif valinta == "C":
+        print("Kertolasku:" , a * b)
+    elif valinta == "D":
+        print("Desimaalijakolasku:" , a / b)
+    else:
+        print("Valintasi on virheellinen.")
