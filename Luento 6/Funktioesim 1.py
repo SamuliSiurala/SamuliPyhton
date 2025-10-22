421aeab208b8e2c7a15e04b2747c062fc65dec04
Samuli Siurala <samppa.siurala@hotmail.com>
2025-10-22 18:47:08 +0300

    Luentoa 22.10.2025

--- Luento 6/Funktioesim 1.py
+++ Luento 6/Funktioesim 1.py
@@ -3,9 +3,15 @@ def terve(kerrat):
         print("Tervehdys!")
     return
 
+def yhteenlasku(luku1,luku2):
+    summa = luku1 + luku2
+    return summa
+
 #Pääohjelma
 print("Uusi päivä alkaa tervehdyksellä: ")
 terve(3)
 print("Jatketaan eteenpäin!")
 terve(5)
+yhteenlasku(3,6)
+
 
