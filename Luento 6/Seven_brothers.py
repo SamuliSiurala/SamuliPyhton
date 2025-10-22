d25bf6409cb20eeac94fca1ab8f7105c4f5de667
Samuli Siurala <samppa.siurala@hotmail.com>
2025-10-22 19:52:35 +0300

    Luentoa 22.10.2025

--- Luento 6/Seven_brothers.py
+++ Luento 6/Seven_brothers.py
@@ -1,5 +1,11 @@
 def seven_brothers(lista):
     #järjestetään lista aakkosjärjestykseen
+    lista.sort()
+
+    #Tulostetaan nimet
+    print(lista)
+    return
 
 #Pääohjelma
 veljekset = ["Juhani", "Lauri", "Aapo", "Timo", "Tuomas", "Eero", "Simeoni"]
+seven_brothers(veljekset)
