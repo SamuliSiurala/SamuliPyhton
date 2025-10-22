8613d0b5510bf9e343e31b46aaa0effd258e759b
Samuli Siurala <samppa.siurala@hotmail.com>
2025-10-22 19:08:22 +0300

    Luentoa 22.10.2025

---
+++ Luento 6/Funktioesim 2.py
@@ -0,0 +1,11 @@
+#Funktiomääritelmät
+def muuta():
+    city = "Vantaa"
+    print("Funktion päätteeksi kaupungin nimi on: " + city)
+    return
+
+#Pääohjelma
+city = "Helsinki"
+print("Pääohjelman alussa kaupungin nimi on: " + city)
+muuta()
+print("Pääohjelman lopussa kaupungin nimi on: " + city)
