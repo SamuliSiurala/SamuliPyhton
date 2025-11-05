03e56c1fcfae6d0ff2360567321394c47683afcd
Samuli Siurala <samppa.siurala@hotmail.com>
2025-11-05 17:58:32 +0200

    Luentoa 5.11.2025

---
+++ Luento 7/7 jotain.py
@@ -0,0 +1,14 @@
+def parilliset(l):
+    uusi_lista = []
+    for luku in l:
+        uusi_lista.append(luku)
+        
+            uusi_lista.append(luku)
+    return uusi_lista
+
+("Pääohjelma"
+ luvut = [5, 12, 3, 44, 8, 17, 0]
+ luvut2 = parilliset(luvut)
+
+print("Alkuperäinen lista:", luvut)
+print("Parillinen lista:", luvut2)
