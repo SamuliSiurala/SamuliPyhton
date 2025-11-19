b9f7fe9eb852e5922c19ea6b302e2182951a01af
Samuli Siurala <samppa.siurala@hotmail.com>
2025-11-19 17:20:57 +0200

    Luentoa 19.11.2025

---
+++ Luento 8/8_1.py
@@ -0,0 +1,7 @@
+vuodenajat = ("talvi", "talvi", "kevät", "kevät", "kevät", "kesä", "kesä", "kesä", "syksy", "syksy", "syksy", "talvi")
+
+kk = int(input("Amma kuukauden numero (1-12): "))
+
+vuodenaika = vuodenajat[kk -1]
+
+print("Vuodenaika on", vuodenaika)
