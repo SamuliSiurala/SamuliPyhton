a27575c589b94f79c4dd68f011e45cdcc575808e
Samuli Siurala <samppa.siurala@hotmail.com>
2025-12-01 15:21:46 +0200

    Tenttiin kokeilua 1.12-25

---
+++ Tenttiin kokeilua/3_1.py
@@ -0,0 +1,8 @@
+password = input("Please type in a password: ")
+
+if password == "kittycat":
+    print("You knew the password!")
+    print("You must be the intended user...")
+    print("...or quite an accomplished hacker.")
+
+print("The program has finished its execution. Thanks and bye!")
