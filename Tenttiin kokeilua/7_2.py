67b43d0ecf281390632a4a55bf6f1115f628c372
Samuli Siurala <samppa.siurala@hotmail.com>
2025-12-01 21:22:20 +0200

    Tenttiin kokeilua 1.12-25

---
+++ Tenttiin kokeilua/7_2.py
@@ -0,0 +1,8 @@
+def make_coordinates(x, y):
+    return (x, y)
+
+xcoordinate = int(input("Give the x coordinate: "))
+ycoordinate = int(input("Give the y coordinate: "))
+
+point = make_coordinates(xcoordinate, ycoordinate)
+print(point)
