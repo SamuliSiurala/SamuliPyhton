d927b77be8e3254050fc005d5e1f7645be3ebc8c
Samuli Siurala <samppa.siurala@hotmail.com>
2025-12-01 19:52:17 +0200

    Tenttiin kokeilua 1.12-25

---
+++ Tenttiin kokeilua/3_3.py
@@ -0,0 +1,4 @@
+height =int(input("How tall are you? "))
+
+if 170 <= height < 180:
+    print("You are tall!")
---
+++ Tenttiin kokeilua/3_4.py
@@ -0,0 +1,12 @@
+
+a = True
+b = False
+
+if a & b:
+    print("Both are true.")
+
+if a | b:
+    print("Either of them, or both, are true.")
+
+if not a & b:
+    print("Neither are true")
