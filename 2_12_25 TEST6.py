eb64586ca051bf127940c650822c54ea57bd02b6
Samuli Siurala <samppa.siurala@hotmail.com>
2025-12-02 16:13:33 +0200

    Tenttiin kokeilua 2.12-25

--- 2_12_25 TEST6.py
+++ 2_12_25 TEST6.py
@@ -1,2 +1,11 @@
+ikä = int(input("Anna ikäsi: "))
+if ikä >= 65:
+    print("Olet eläkkeellä.")
+elif ikä >= 18:
+    print("Olet työikäinen.")
+elif ikä >= 7:
+    print("Olet koululainen.")
+else:
+    print("Olet lapsi.")
 
 
