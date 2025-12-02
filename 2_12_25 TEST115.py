01d20286f584917465f7d482578742323cdba230
Samuli Siurala <samppa.siurala@hotmail.com>
2025-12-02 19:01:51 +0200

    Tenttiin kokeilua 2.12-25

--- 2_12_25 TEST115.py
+++ 2_12_25 TEST115.py
@@ -1,4 +1,11 @@
+nimet = []
 
+nimi = input("Anna ensimmäinen nimi tai lopeta painamalla Enter: ")
+while nimi != "":
+    nimet.append(nimi)
+    nimi = input("Anna seuraava nimi tai lopeta painamalla Enter: ")
+
+print(nimet)
 
 
 
