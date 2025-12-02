756604b76ffb2bc99e1247c35353f1c2b974c209
Samuli Siurala <samppa.siurala@hotmail.com>
2025-12-02 18:17:10 +0200

    Tenttiin kokeilua 2.12-25

--- 2_12_25 TEST113.py
+++ 2_12_25 TEST113.py
@@ -1,4 +1,12 @@
-
+komento = input("Anna komento: ")
+while komento != "lopeta!":
+    if komento == "MAYDAY!":
+        break
+    print("Suoritan toiminnon: " + komento)
+    komento = input("Anna komento: ")
+else:
+    print("Näkemiin!")
+print("Toiminnot lopetettu.")
 
 
 
