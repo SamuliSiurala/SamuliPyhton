cee1c7dc2fa6e75d5e91cc0115c9936f26998584
Samuli Siurala <samppa.siurala@hotmail.com>
2025-12-02 11:47:26 +0200

    Tenttiin kokeilua 2.12-25

--- 2_12_25 TEST2.py
+++ 2_12_25 TEST2.py
@@ -1,5 +1,4 @@
-fahrenheit_str = input("Anna lämpötila Fahrenheit-asteina: ")
-fahrenheit = float(fahrenheit_str)
-celsius = (fahrenheit-32)*5/9
-print(f"Lämpötila Celsius-asteina: {celsius:.2f}")
+import.math
+
+
 
