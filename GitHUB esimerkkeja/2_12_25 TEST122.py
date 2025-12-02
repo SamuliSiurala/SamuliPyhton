3c138dbb69b89d4a3547d6ba3d229a1e659230c6
Samuli Siurala <samppa.siurala@hotmail.com>
2025-12-02 19:58:37 +0200

    Tenttiin kokeilua 2.12-25

---
+++ GitHUB esimerkkeja/2_12_25 TEST122.py
@@ -0,0 +1,11 @@
+def inventaario(tavarat):
+    print("Sinulla on seuraavat tavarat: ")
+    for t in tavarat:
+        print("-" + t)
+    return
+
+reppu = ["Vesipullo", "Kartta", "Kompassi"]
+inventaario(reppu)
+reppu.append("Linkkuveitsi")
+inventaario(reppu)
+
