d6eb4c645f34216daa497a047660e2faee7d6f1e
Samuli Siurala <samppa.siurala@hotmail.com>
2025-12-02 16:57:55 +0200

    Tenttiin kokeilua 2.12-25

--- 2_12_25 TEST8.py
+++ 2_12_25 TEST8.py
@@ -1,3 +1,9 @@
+komento = input("Anna komento: ")
+while komento != "lopeta":
+    print("Suoritan toiminnon: " + komento)
+    komento = input("Anna komento: ")
+print("Toiminnot lopetettu.")
+
 
 
 
