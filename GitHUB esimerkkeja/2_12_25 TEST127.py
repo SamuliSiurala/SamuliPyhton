fbfd2d681aa5b32a9f322e328370681d1fb95dc7
Samuli Siurala <samppa.siurala@hotmail.com>
2025-12-02 20:47:41 +0200

    Tenttiin kokeilua 2.12-25

--- GitHUB esimerkkeja/2_12_25 TEST127.py
+++ GitHUB esimerkkeja/2_12_25 TEST127.py
@@ -1,2 +1,15 @@
+pelit = {"Monopoli", "Shakki", "Cluedo"}
+print(pelit)
 
+pelit.add("Dominion")
+print(pelit)
+
+pelit.remove("Shakki")
+print(pelit)
+
+pelit.add("Cluedo")
+print(pelit)
+
+for p in pelit:
+    print(p)
 
