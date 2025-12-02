62ebd00c9ca29e28f5df26d92a5e9f57ca9d7cc8
Samuli Siurala <samppa.siurala@hotmail.com>
2025-12-02 19:39:09 +0200

    Tenttiin kokeilua 2.12-25

---
+++ GitHUB esimerkkeja/2_12_25 TEST120.py
@@ -0,0 +1,11 @@
+def vaihda():
+    kaupunki = "Vantaa"
+    print("Funktiossa lopuksi: " + kaupunki)
+    return
+
+kaupunki = "Helsinki"
+print("Pääohjelmassa aluksi: " + kaupunki)
+vaihda()
+print("Pääohjelmassa lopuksi: " + kaupunki)
+
+
