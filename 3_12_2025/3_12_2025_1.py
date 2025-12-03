b8c76fef32b02c6d026c0fb57294024c6c9d00cb
Samuli Siurala <samppa.siurala@hotmail.com>
2025-12-03 16:04:05 +0200

    Tenttiin kokeilua 2.12-25

--- 3_12_2025/3_12_2025_1.py
+++ 3_12_2025/3_12_2025_1.py
@@ -1,2 +1,5 @@
-käyttäjä = input("Anna nimesi: ")
-print("Hauska tavata, " + käyttäjä + "!")
+ikä = int(input("Anna ikä: "))
+if 15 <= ikä < 18:
+    paino = float(input("Anna paino (kg): "))
+if (ikä >= 18 or ikä >= 15 and paino >= 55):
+    print("Lääkkeen käyttö sallittua.")
